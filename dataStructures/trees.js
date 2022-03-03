5c42a59d2d333a9d38d10fb4eb23b2083996292d
Miles Marillo <mjm03@hampshire.edu>
2022-03-03 13:46:14 -0500

    trees

--- dataStructures/trees.js
+++ dataStructures/trees.js
@@ -10,5 +10,16 @@ class BinarySearchTree {
     constructor() {
         this.root = null
     }
-    
+
+    insert(val) {
+        let newNode = new Node(val)
+        if (!this.root) {
+            this.root = newNode
+            return this
+        }
+        if (newNode.val > this,root,val && this.root.right) {
+            this.insert(this.root.right)
+        }
+        if (!this.root.left) this.root.left = newNode
+    }
 }
